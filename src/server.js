dc4a8a882a04d4d42dd1ebcc829bd04512ffea29
Alex Homoky <zhomoky@gmail.com>
2024-02-06 22:41:23 +0000

    chore: cleanup

--- src/server.js
+++ src/server.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use("/example", express.static("example"));
-
-app.use("/dogs", express.static("dogs"));
-
 app.listen(5001, () => {
   console.log("Server is listening on 5001");
 });
