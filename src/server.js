b6557d93bf7f8c1db7e2edba30a1373030feb077
Alex Homoky <zhomoky@gmail.com>
2024-02-06 21:04:57 +0000

    a commit

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use("/example", express.static("example"));
 
+app.use("/dogs", express.static("dogs"));
+
 app.listen(5001, () => {
   console.log("Server is listening on 5001");
 });
