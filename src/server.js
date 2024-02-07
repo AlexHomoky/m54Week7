2c3c9f697fe335afa684d5f50e186465270c7b44
Alex Homoky <zhomoky@gmail.com>
2024-02-07 17:10:45 +0000

    mongo updates

--- src/server.js
+++ src/server.js
@@ -1,26 +1,55 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-//HTTP Verbs - GET, POST, PUT, DELETE
-
-// const response = await fetch("http://someaddress.com"); //sends GET request
-
-// HTTP Verb GET
-
-const fakeArr = [];
-
 app.use(express.json());
 
-app.get("/getAllBooks", (request, response) => {
-  response.send({ message: "success", fakeArr: fakeArr });
+const connection = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("DB connection is working");
+};
+
+connection();
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+  },
+  genre: {
+    type: String,
+  },
 });
 
-app.post("/addBook", (request, response) => {
-  fakeArr.push(request.body);
-  console.log(fakeArr);
-  response.send({ message: "success", newBook: fakeArr[fakeArr.length - 1] });
+const Book = mongoose.model("Book", bookSchema);
+
+app.get("/getAllBooks", async (request, response) => {
+  //get all books from db
+  const books = await Book.find({});
+  response.send({ message: "success on all the books.", books: books });
 });
+
+app.post("/books/addBook", async (request, response) => {
+  //add single book to db
+  console.log("request.body", request.body);
+  const book = await Book.create({
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre,
+  });
+  response.send({ message: "success book created", book: book });
+  console.log("book: ", book);
+});
+
+app.put("/books", (request, response) => {});
+
+app.delete("/books", (request, response) => {});
 
 app.listen(5001, () => {
   console.log("Server is listening on 5001");
