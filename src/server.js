8894edd9429338d28442733044cb6a92f0902300
Alex Homoky <zhomoky@gmail.com>
2024-02-08 13:40:49 +0000

    4 main tasks

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+const fakeArr = [];
+
 app.use(express.json());
 
 const connection = async () => {
@@ -33,6 +35,13 @@ app.get("/getAllBooks", async (request, response) => {
   //get all books from db
   const books = await Book.find({});
   response.send({ message: "success on all the books.", books: books });
+  //await logTypeOfResult(books);
+});
+
+app.get("/getFirstBook", async (request, response) => {
+  //get first book
+  //   const book = await Book.find({book.[0]});
+  //   response.send({ message: "success first book", book: book });
 });
 
 app.post("/books/addBook", async (request, response) => {
@@ -47,9 +56,21 @@ app.post("/books/addBook", async (request, response) => {
   console.log("book: ", book);
 });
 
-app.put("/books", (request, response) => {});
+app.put("/books", async (request, response) => {
+  // in here, find a book by title (i.e. an element of fakeArr where the element title is the same as request.body.title)
+  // change (update) the author to an new name
+  const filter = { author: "graham" };
+  const update = { author: "toddley" };
 
-app.delete("/books", (request, response) => {});
+  let book = await Book.findOneAndUpdate(filter, update, { new: true });
+  response.send({ message: "success author replaced", book: book });
+  console.log("book: ", book);
+});
+
+app.delete("/deleteBook", async (request, response) => {
+  let book = await Book.deleteOne({ author: "barbara" });
+  response.send({ message: "successful deletion" });
+});
 
 app.listen(5001, () => {
   console.log("Server is listening on 5001");
